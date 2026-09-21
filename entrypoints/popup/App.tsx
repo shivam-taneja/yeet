32356c0926e44483489eaff9790608a27cce844c
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-21 22:29:15 +0530

    feat: integrate Tailwind CSS and shadcn/ui components

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,9 +9,12 @@ function App() {
         <a href="https://wxt.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank"></a>
       </div>
-      <h1>WXT + React</h1>
+      <h1 className="bg-yellow-500">WXT + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-red-500"
+        >
           count is {count}
         </button>
         <p>
