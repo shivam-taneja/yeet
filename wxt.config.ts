ea717a9c8cf2b41903f3331cfb1ae0af3f9911df
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-21 22:15:50 +0530

    chore: rebrand template to Yeet and add auto-icons support

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
+  autoIcons: {
+    developmentIndicator: "overlay",
+  },
+  webExt: {
+    disabled: true,
+  },
 });
