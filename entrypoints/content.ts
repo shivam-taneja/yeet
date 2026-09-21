7cba685031fc15147ac69d7221ff983bb7f6ccfa
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-21 22:54:34 +0530

    chore: update content script log message

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,8 @@
 export default defineContentScript({
   matches: ["*://*.x.com/*", "*://*.twitter.com/*", "*://*.threads.com/*"],
   main() {
-    console.log("[Yeet] Content script active.");
+    console.log(
+      "[Yeet] extension woke up. nobody asked. both apps will suffer.",
+    );
   },
 });
